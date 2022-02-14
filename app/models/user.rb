544cbe7a2a05e5965e31b13afdc84086d9c485fe
m.motonaga <mockysunshinework@gmail.com>
2022-02-14 14:45:07 +0000

    4.5 パスワードを追加しよう

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  
+  has_secure_password
+  validates :password, presence: true, lengh: { minimum: 6 }
 end
