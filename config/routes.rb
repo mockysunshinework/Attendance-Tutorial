8a6c2c6a3518e83f12f5e796d70ca8cc7b062560
m.motonaga <mockysunshinework@gmail.com>
2022-03-01 02:11:53 +0000

    10.1.1 【1対多】の関連付け

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
-  
+
   # ログイン機能
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete  '/logout', to: 'sessions#destroy'
-  
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users do
-    member do 
+    member do
       get 'edit_basic_info'
       patch 'update_basic_info'
     end
